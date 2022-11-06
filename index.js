01fb43afd44b6b313308b9c12fa3d295778e2de9
kheanglov <lovsokheang@gmail.com>
2022-11-06 14:06:18 +0700

    wqeqwesad

--- index.js
+++ index.js
@@ -37,11 +37,11 @@ app.get("/mp3", async (req, res) => {
     title = encodeURIComponent(`${title}.mp3`);
 
     // res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Origin', '*')
     // another common pattern
     // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
+    res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.header(
       'Access-Control-Allow-Headers',
       'Origin', 'Authorization', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
     )
@@ -81,11 +81,11 @@ app.get("/mp4", async (req, res) => {
     title = encodeURIComponent(`${title}.mp3`);
 
     // res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Origin', '*')
     // another common pattern
     // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
+    res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.header(
       'Access-Control-Allow-Headers',
       'Origin', 'Authorization', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
     )
@@ -106,7 +106,3 @@ app.get("/mp4", async (req, res) => {
     });
   }
 });
-
-const log = (...msg) => {
-  console.log(msg);
-};
